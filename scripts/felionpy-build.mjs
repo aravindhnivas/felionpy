67a723f54933eeece8a2f891825859c0b247c00a
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-01-09 15:03:57 +0100

    initial commit

---
+++ scripts/felionpy-build.mjs
@@ -0,0 +1,18 @@
+import { spawn } from 'child_process'
+import path from 'path'
+const maindir = path.resolve("./")
+const distpath = path.join(maindir, 'build')
+const icon = path.join(maindir, 'icons/icon.ico')
+const hooks = path.join(maindir, 'hooks')
+const mainfile = path.join(maindir, 'main.py')
+const args =
+    `--noconfirm --onedir --console --icon ${icon} --name felionpy --debug noarchive --noupx --additional-hooks-dir ${hooks} --hidden-import felionlib --paths ${maindir} --distpath ${distpath} ${mainfile}`.split(
+        ' '
+    )
+console.log(args)
+
+const py = spawn('pyinstaller', args)
+py.stdout.on('data', (data) => console.log(data.toString('utf8')))
+py.stderr.on('data', (data) => console.log(data.toString('utf8')))
+py.on('close', () => console.log('closed'))
+py.on('error', (err) => console.log('error occured', err))
