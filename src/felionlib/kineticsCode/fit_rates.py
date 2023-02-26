fc1a8105de83bb7ea8a9c1ad0293aba9d0849e74
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-02-26 23:10:22 +0100

    Update fit_rates.py

--- src/felionlib/kineticsCode/fit_rates.py
+++ src/felionlib/kineticsCode/fit_rates.py
@@ -1,16 +1,41 @@
+import numpy as np
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
-import numpy as np
 from scipy.optimize import curve_fit
+from felionlib.utils.FELion_definitions import get_weighted_mean_from_uarray
 
 
 def main(args):
     # print(f"{args=}", flush=True)
     polyOrder = float(args["polyOrder"])
-    addIntercept = bool(args["addIntercept"])
+
     fitted_values = args["fitted_values"]
     number_densities = args["number_densities"]
 
+    x = np.array(number_densities["val"], dtype=float)
+    x_err = np.array(number_densities["std"], dtype=float)
+    unp_x = unp.uarray(x, x_err)
+
+    y = np.array(fitted_values["val"], dtype=float)
+    y_err = np.array(fitted_values["std"], dtype=float)
+    unp_y = unp.uarray(y, y_err)
+
+    # print(f"{unp_x=}", flush=True)
+    rate_constant = unp_y / unp_x**polyOrder
+
+    dataToSend = {
+        "rate_constant": {
+            "val": unp.nominal_values(rate_constant).tolist(),
+            "std": unp.std_devs(rate_constant).tolist(),
+            "mean": f"{ufloat(unp.nominal_values(rate_constant).mean(), unp.nominal_values(rate_constant).std())}",
+            "weighted_mean": f"{get_weighted_mean_from_uarray(rate_constant, ufloat_fmt=True)}",
+        },
+    }
+
+    if not args["fit"]:
+        return dataToSend
+
+    addIntercept = bool(args["addIntercept"])
     rate_constant_guess = float(args["$rate_constant_guess"])
 
     # print(f"{fitted_values['val']=}", flush=True)
@@ -19,11 +44,6 @@ def main(args):
         if addIntercept:
             return m * x**polyOrder + c
         return m * x**polyOrder
-
-    x = np.array(number_densities["val"])
-
-    y = np.array(fitted_values["val"])
-    y_err = np.array(fitted_values["std"])
 
     if addIntercept:
         intercept_guess = float(args["$intercept_guess"])
@@ -49,7 +69,7 @@ def main(args):
     print(f"{upop=}", flush=True)
     print(f"{fitY=}", flush=True)
 
-    dataToSend = {
+    dataToSend = dataToSend | {
         "fitY": {
             "val": unp.nominal_values(fitY).tolist(),
             "std": unp.std_devs(fitY).tolist(),
