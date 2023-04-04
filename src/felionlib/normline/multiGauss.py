734b8da2fbb0a32c15b578d90ad8410ce30fdfa2
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-04-04 23:46:43 +0200

    Update multiGauss.py

--- src/felionlib/normline/multiGauss.py
+++ src/felionlib/normline/multiGauss.py
@@ -153,7 +153,6 @@ def main(gauss_args):
             method = norm_method
         else:
             dataTosend[method] = {}
-
         print(f"Fitting for method: {method}\n")
 
         if felix:
